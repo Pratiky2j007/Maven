f66a9d71fe1b31ebb9cbea2ad36e69b3aab644f0
pkumar <pkumar@pkumar-lt.wk.pune>
2019-01-22 18:08:30 +0530

    Adding Project to branch

---
+++ FirstMavenProject1/src/test/java/test/LoginTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest
+{
+	
+	@Test
+	public void LoginTestMethod()
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+		WebDriver fDriver=new FirefoxDriver();
+		fDriver.get("https://opensource-demo.orangehrmlive.com/");
+		fDriver.findElement(By.id("txtUsername")).sendKeys("admin");
+		fDriver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		fDriver.findElement(By.id("btnLogin")).click();
+	}
+
+}
