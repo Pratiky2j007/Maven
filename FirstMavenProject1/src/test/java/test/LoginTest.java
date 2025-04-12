af59834725885854ebdb98a247f842f1d8838f08
Pratiky2j007 <Pratik.y2j007@gmail.com>
2025-04-12 12:45:30 +0530

    added one line by Pratik

--- FirstMavenProject1/src/test/java/test/LoginTest.java
+++ FirstMavenProject1/src/test/java/test/LoginTest.java
@@ -24,6 +24,7 @@ public class LoginTest
 		System.out.println("adding one line code github--aditya");
 		
 		System.out.println("Adding one line for github-Pratik");
+		System.out.println("Adding one line - Pratik");
 		
 	}
    
